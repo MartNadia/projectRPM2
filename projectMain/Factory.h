9281dbbdbdebc6423e61dd5f64a6e10dac8829af
MartNadia <106438278+MartNadia@users.noreply.github.com>
2025-06-26 10:55:32 +0300

    Update Factory.h

--- projectMain/Factory.h
+++ projectMain/Factory.h
@@ -16,35 +16,30 @@
 #include <vector>
 #include <iostream>
 
-// Forward declaration for EmployeeManagementSystem
 class EmployeeManagementSystem;
 
 
 class Factory {
 public:
-    static Employee* createEmployee(EmployeeType type, size_t id, const std::string& name, size_t worktime,
+    static Employee* createEmployee(EmployeeType type, size_t id, const string& name, size_t worktime,
         double arg1 = 0.0, double arg2 = 0.0, double arg3 = 0.0, size_t arg4 = 0, size_t arg5 = 0);
 
-    static EmployeeType getEmployeeTypeFromString(const std::string& typeString);
-    static EmployeeType getEmployeeTypeFromStringRegular(const std::string& typeString);
+    static EmployeeType getEmployeeTypeFromStringRegular(const string& typeString);
 
-    // --- File I/O ---
-    static bool loadEmployeesFromFile(EmployeeManagementSystem& system, const std::string& filename);
-    static bool saveEmployeesToFile(const EmployeeManagementSystem& system, const std::string& filename);
+    static bool loadEmployeesFromFile(EmployeeManagementSystem& system, const string& filename);
+    static bool loadProjectsFromFile(EmployeeManagementSystem& system, const string& filename);
+    static bool saveProjectsToFile(const EmployeeManagementSystem& system, const string& filename);
+    static bool saveEmployeesToFile(const EmployeeManagementSystem& system, const string& filename);
 
-    // --- Search Functions ---
-    static std::vector<Employee*> findEmployeesByName(const EmployeeManagementSystem& system, const std::string& name);
-    static std::vector<Employee*> findEmployeesByPosition(const EmployeeManagementSystem& system, EmployeeType position);
-    static std::vector<Employee*> findEmployeesByProject(const EmployeeManagementSystem& system, const std::string& projectName);
-    static std::vector<Employee*> findEmployeesBySalary(const EmployeeManagementSystem& system, double salary, bool greaterThan);
+    static vector<Employee*> findEmployeesByName(const EmployeeManagementSystem& system, const string& name);
+    static vector<Employee*> findEmployeesByPosition(const EmployeeManagementSystem& system, EmployeeType position);
+    static vector<Employee*> findEmployeesByProject(const EmployeeManagementSystem& system, const string& projectName);
+    static vector<Employee*> findEmployeesBySalary(const EmployeeManagementSystem& system, double salary, bool greaterThan);
 
 private:
-    Factory() {} // Private constructor to prevent instantiation
-    // Helper function to convert EmployeeType to string (for saving)
-    static std::string employeeTypeToString(EmployeeType type);
-    // Helper function to convert string to EmployeeType (for loading)
-    static EmployeeType stringToEmployeeType(const std::string& typeString);
+    Factory() {}
+    static string employeeTypeToString(EmployeeType type);
+    static EmployeeType stringToEmployeeType(const string& typeString);
 };
 
-// Overload the << operator for EmployeeType
-std::ostream& operator<<(std::ostream& os, EmployeeType type);
+ostream& operator<<(ostream& os, EmployeeType type);
