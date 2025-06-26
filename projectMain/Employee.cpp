855122a505e2b184f3b384eeb2c89d63a090bcad
MartNadia <106438278+MartNadia@users.noreply.github.com>
2025-06-26 10:54:55 +0300

    Update Employee.cpp

--- projectMain/Employee.cpp
+++ projectMain/Employee.cpp
@@ -1,10 +1,24 @@
 #include "Employee.h"
 
-Employee::Employee() : id(0), name(""), worktime(0), hourlyRate(0.0) {}
+Employee::Employee() : id(0), name(""), worktime(0), hourlyRate(0.0), employeeType(EmployeeType::UNKNOWN) {}
 
-Employee::Employee(size_t id, const string& name, size_t worktime)
-	: id(id), name(name), worktime(worktime), hourlyRate(0.0) {}
+Employee::Employee(size_t id, const string& name, size_t worktime, EmployeeType type)
+	: id(id), name(name), worktime(worktime), hourlyRate(0.0), employeeType(type) {}
 
+string employeeTypeToString(EmployeeType type) {
+    switch (type) {
+    case EmployeeType::PERSONAL:      return "Personal";
+    case EmployeeType::ENGINEER:      return "Engineer";
+    case EmployeeType::CLEANER:       return "Cleaner";
+    case EmployeeType::DRIVER:        return "Driver";
+    case EmployeeType::PROGRAMMER:    return "Programmer";
+    case EmployeeType::TESTER:        return "Tester";
+    case EmployeeType::TEAMLEADER:    return "Team Leader";
+    case EmployeeType::PROJECTMANAGER: return "Project Manager";
+    case EmployeeType::SENIORMANAGER: return "Senior Manager";
+    default:                          return "UNKNOWN";
+    }
+}
 size_t Employee::getId() const { return id; }
 
 string Employee::getName() const { return name; }
@@ -21,8 +35,17 @@ void Employee::setWorktime(size_t worktime) { this->worktime = worktime; }
 
 void Employee::setHourlyRate(double hourlyRate) { this->hourlyRate = hourlyRate; }
 
+EmployeeType Employee::getEmployeeType() const {
+	return employeeType;
+}
+
+void Employee::setEmployeeType(EmployeeType type) {
+	employeeType = type;
+}
+
 void Employee::print() const {
 	cout << "ID: " << id << endl;
+	cout << "Employee Type: " << employeeTypeToString(getEmployeeType()) << endl;
 	cout << "Name: " << name << endl;
 	cout << "Worktime: " << worktime << endl;
 }
